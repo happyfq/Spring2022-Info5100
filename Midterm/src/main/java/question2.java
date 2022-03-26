ff9b7310e4f42838e4b12d491433a2bdc5874ecb
happyfq <651408115@qq.com>
2022-03-26 16:38:25 -0700

    Midterm

--- Midterm/src/main/java/question2.java
+++ Midterm/src/main/java/question2.java
@@ -22,24 +22,3 @@ can have data filed;
 can declare constructors or destructors;
 
  */
-/**
- * difference between abstract class and interface
- *
- * 1.abstract class: "is a" relation class(mostly describe something's natual thing)
- *   interface: "has a" relation with class(mostly describe something's functions)
- *
- * 2.abstract class:could realize some methods
- *   interface: totally abstract, do not realize any methods
- *
- * 3.abstract class: son-class realize it in "extends"
- *    interface: use implements
- *
- * 4.abstract class:could have constructors
- *   interface: no constructors
- *
- * 5.abstract class: could have main method
- *  interface: could not have main method
- *
- * 6.abstract class: faster than interface
- *  interface:slower than abstract class
- */
